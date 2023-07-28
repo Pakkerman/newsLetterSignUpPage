fddca2a7ff2122e9b19911f27ad857908d11f985
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2023-07-28 12:34:56 +0800

    basic layout complete

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <div className="pre fixed bottom-0">
+      <div className="pre relative ">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Pakkerman</a>.
+      </div>
+    </div>
+  );
+}
