77937f0dca51b616fdc7151c514ba81c4ed8bf2f
Pakkerman <104409243+Pakkerman@users.noreply.github.com>
2023-07-31 10:58:20 +0800

    Layout and success page for both mobile and desktop

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,9 +3,7 @@ import { PropsWithChildren } from "react"
 export default function layout(props: PropsWithChildren) {
   return (
     <main className="flex h-[100svh] min-h-[768px] w-full flex-col items-center justify-center bg-slate-100 sm:bg-slate-700 ">
-      <div className=" flex h-full w-full max-w-md flex-col bg-slate-100 sm:h-[80%] sm:w-[80%] sm:max-w-none sm:flex-row-reverse sm:items-center sm:justify-center sm:rounded-3xl sm:p-6">
-        {props.children}
-      </div>
+      {props.children}
     </main>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import Image from "next/image"
 import React, { useState } from "react"
 import Layout from "~/components/Layout"
 import CustomButton from "~/components/CustomButton"
@@ -23,76 +24,81 @@ export default function Home() {
 
   return (
     <Layout>
-      <div className="h-[284px] shrink overflow-hidden rounded-b-2xl sm:hidden">
-        <img
-          className="h-full w-full object-cover"
-          src="assets/images/illustration-mobile.svg"
-          alt="cover image"
-        />
-      </div>
-      <div className="hidden h-full overflow-hidden rounded-3xl sm:block">
-        <img
-          className="h-full w-full object-cover object-right"
-          src="assets/images/illustration-desktop.svg"
-          alt="cover image"
-        />
-      </div>
-      <section className=" flex grow flex-col justify-evenly px-8 sm:items-center sm:space-y-6">
-        <h1 className="w-full font-RobotoBold text-4xl sm:text-[5vw] sm:leading-[4vw]">
-          Stay updated!
-        </h1>
-        <h1 className="w-full">
-          Join 60,000+ product managers receiving monthly updates on:
-        </h1>
-        <ul className="w-full space-y-4">
-          <li className="flex items-start space-x-4 ">
-            <img src="assets/images/icon-list.svg" alt="list icon" />
-            <p>Product discovery and building what matters</p>
-          </li>
-          <li className="flex items-start space-x-4">
-            <img src="assets/images/icon-list.svg" alt="list icon" />
-            <p>Measuring to ensure updates are a success</p>
-          </li>
-          <li className="flex items-start space-x-4">
-            <img src="assets/images/icon-list.svg" alt="list icon" />
-            <p>And much more!</p>
-          </li>
-        </ul>
+      <div className=" flex h-full w-full max-w-md flex-col bg-slate-100 sm:h-[80%] sm:w-[80%] sm:max-w-5xl sm:flex-row-reverse sm:items-center sm:justify-center sm:rounded-3xl sm:p-6">
+        <div className="h-[284px] shrink overflow-hidden rounded-b-2xl sm:hidden">
+          <Image
+            width={500}
+            height={500}
+            className="h-full w-full object-cover"
+            src="assets/images/illustration-mobile.svg"
+            alt="cover image"
+          />
+        </div>
+        <div className="hidden h-full overflow-hidden rounded-3xl md:block">
+          <img
+            className="h-full w-full object-cover object-right"
+            src="assets/images/illustration-desktop.svg"
+            alt="cover image"
+          />
+        </div>
+        <section className=" flex grow flex-col justify-evenly px-8 sm:items-start sm:space-y-6">
+          <h1 className="w-full font-RobotoBold text-4xl sm:text-[5vw] sm:leading-[4vw]">
+            Stay updated!
+          </h1>
+          <h1 className="w-full">
+            Join 60,000+ product managers receiving monthly updates on:
+          </h1>
+          <ul className="w-full space-y-4">
+            <li className="flex items-start space-x-4 ">
+              <img src="assets/images/icon-list.svg" alt="list icon" />
+              <p>Product discovery and building what matters</p>
+            </li>
+            <li className="flex items-start space-x-4">
+              <img src="assets/images/icon-list.svg" alt="list icon" />
+              <p>Measuring to ensure updates are a success</p>
+            </li>
+            <li className="flex items-start space-x-4">
+              <img src="assets/images/icon-list.svg" alt="list icon" />
+              <p>And much more!</p>
+            </li>
+          </ul>
 
-        <form
-          className="flex w-full max-w-lg flex-col justify-center space-y-4 "
-          onSubmit={validate}
-        >
-          <div className="flex flex-col space-y-2">
-            <div className="flex justify-between opacity-90">
-              <label className="font-RobotoBold text-xs ">Email Address</label>
-              {!isValid && (
-                <p className=" text-xs font-RobotoBold text-red-500">
-                  Valid Email Required
-                </p>
-              )}
-            </div>
-            <input
-              type="text"
-              onChange={handleEmailChange}
-              value={email}
-              placeholder="email@company.com"
-              className={`h-12 rounded-md border-[1px] p-3 shadow-inner
-                hover:cursor-pointer outline-none
+          <form
+            className="flex w-full max-w-lg flex-col justify-center space-y-4 "
+            onSubmit={validate}>
+            <div className="flex flex-col space-y-2">
+              <div className="flex justify-between opacity-90">
+                <label className="font-RobotoBold text-xs ">
+                  Email Address
+                </label>
+                {!isValid && (
+                  <p className=" font-RobotoBold text-xs text-red-500">
+                    Valid Email Required
+                  </p>
+                )}
+              </div>
+              <input
+                type="text"
+                onChange={handleEmailChange}
+                value={email}
+                placeholder="email@company.com"
+                className={`h-12 rounded-md border-[1px] p-3 shadow-inner
+                outline-none transition-all
+                hover:cursor-pointer
                 focus:border-opacity-100
-                transition-all
                 ${
                   isValid
                     ? "border-slate-800 border-opacity-40 "
                     : "border-red-500 border-opacity-100 bg-red-100 bg-opacity-60"
                 }`}
-            />
-          </div>
-          <Link href="/success">
-            <CustomButton buttonText=" Subscribe to monthly newsletter" />
-          </Link>
-        </form>
-      </section>
+              />
+            </div>
+            <Link href="/success">
+              <CustomButton buttonText=" Subscribe to monthly newsletter" />
+            </Link>
+          </form>
+        </section>
+      </div>
     </Layout>
   )
 }
